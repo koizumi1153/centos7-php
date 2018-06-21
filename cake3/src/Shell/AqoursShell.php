919bcd58004870cf9a22a041d6aaeb6cb66ad75f
小泉 憲弘 <koizumi1153+1@gmail.com>
2018-06-21 13:11:58 +0900

    url追加

--- cake3/src/Shell/AqoursShell.php
+++ cake3/src/Shell/AqoursShell.php
@@ -194,6 +194,8 @@ class AqoursShell extends Shell
 
       // title
       $title = trim($dom["#channel-main"]->find("a:eq(1)")->text());
+      $text .= "\n\n".$url;
+
 /*
       $text .= "\n\n";
       // 説明
