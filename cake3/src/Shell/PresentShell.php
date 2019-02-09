3618c675a35f5f55a79fbc5db4b181c9cb2219ca
小泉 憲弘 <koizumi1153+1@gmail.com>
2019-02-09 14:20:36 +0900

    Shell修正

--- cake3/src/Shell/PresentShell.php
+++ cake3/src/Shell/PresentShell.php
@@ -16,44 +16,39 @@ class PresentShell extends Shell
 
     public function main()
     {
-        $img = '';
-        $word = '';
-        $wordId = 0;
-        $useCount = 0;
-
         $now = date('Y-m-d H:i:s');
-        $ids = array();
 
         //時間チェック
         $presents = $this->Present->getDate($now);
         if(!empty($presents)){
             foreach($presents as $present) {
+                $ids = array();
                 //kindチェック
                 if ($present['kind'] == PRESENT_ONCE && $present['retweet_flg'] == ON_FLG) {
                     break;
                 } elseif($present['kind'] == PRESENT_ALL && $present['retweet_flg'] == ON_FLG) {
                     $ids = explode(',', $present['retweet_ids']);
                 }
-            }
 
-            //フォロー処理を入れる
-            $this->Twitter->setFollowScreenName($present['screen_name']);
+                //フォロー処理を入れる
+                $this->Twitter->setFollowScreenName($present['screen_name']);
 
-            //最新のタイムラインを取得する(10件)
-            $timeLines = $this->Twitter->getUserTimeline($present['screen_name'], 5);
+                //最新のタイムラインを取得する(10件)
+                $timeLines = $this->Twitter->getUserTimeline($present['screen_name'], 10);
 
-            //タイムラインチェック
-            foreach($timeLines as $timeLine){
-                //リツイートしたものは除外する
-                if(!in_array($timeLine->id_str, $ids)){
-                    if (strpos($timeLine->text, $present['word']) !== false){
-                        $this->Twitter->retweet($timeLine->id_str);
-                        $ids[] = $timeLine->id_str;
-                        $this->Present->update($present['id'],$ids);
+                //タイムラインチェック
+                foreach($timeLines as $timeLine){
+                    //リツイートしたものは除外する
+                    if(!in_array($timeLine->id_str, $ids)){
+                        if (strpos($timeLine->text, $present['word']) !== false){
+                            $this->Twitter->retweet($timeLine->id_str);
+                            $ids[] = $timeLine->id_str;
+                            $this->Present->update($present['id'],$ids);
+                        }
                     }
                 }
+                sleep(1);
             }
-            sleep(1);
         }
     }
 
